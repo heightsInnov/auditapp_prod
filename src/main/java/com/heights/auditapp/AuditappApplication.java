08ee0f4900db8001a646172569336761468d71bc
aojinadu <ayo02034reel@yahoo.com>
2022-01-19 08:08:38 +0100

    first commit

---
+++ src/main/java/com/heights/auditapp/AuditappApplication.java
@@ -0,0 +1,13 @@
+package com.heights.auditapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heights/auditapp/AuditappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heights.auditapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
